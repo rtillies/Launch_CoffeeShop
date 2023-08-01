4bbb27c360182efb749313ddf960b5966768d1dd
Richard T <richard@turing.edu>
2023-08-01 11:38:45 -0400

    Customer #Show displays total spend and items ordered

--- CoffeeShopMVC/Models/Order.cs
+++ CoffeeShopMVC/Models/Order.cs
@@ -6,5 +6,21 @@
         public DateTime DateCreated { get; set; }
         public List<Item> Items { get; set; } = new List<Item>();
         public Customer Customer { get; set; }
-    }
+
+        public double OrderTotal()
+        {
+            double total = 0.0;
+            foreach(var item in Items)
+            {
+                total += item.PriceInCents;
+            }
+            return total;
+        }
+
+		public string DisplayDollars(int cents)
+		{
+			return Math.Round(cents / 100.0, 2).ToString();
+		}
+
+	}
 }
